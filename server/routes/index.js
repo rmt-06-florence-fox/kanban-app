e783ea2b0015424d15fdb06069b1f4c67ba7c87f
FebrianAditya <febrian.aditya@student.upi.edu>
2020-11-30 21:02:32 +0700

    try again again

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,8 @@ const ControllerUser = require("../controllers/userControllers")
 
 route.post("/register", ControllerUser.registerUser)
 route.post("/login", ControllerUser.loginUser)
+route.get("/", (req, res) => {
+    res.send("-----------------")
+})
 
 module.exports = route
