30292b76df1f0ad8fae50348a0c3227edd334a5f
sangsangga <wandaputrasangga@gmail.com>
2020-11-30 17:26:11 +0700

    first commit

---
+++ server/seeders/20201130094540-category-seeder.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   let categories = require('../data/categories.json')
+   categories.forEach(cat => {
+     cat.createdAt=new Date
+     cat.updatedAt= new Date
+   })
+   await queryInterface.bulkInsert('Categories',categories)
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Categories')
+  }
+};
