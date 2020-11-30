db3208ef575f954123e98ccd0f70d4eb251c75aa
icanq <icanq@.localdomain>
2020-11-30 19:56:46 +0700

    user login ✔

--- server/controllers/userC.js
+++ server/controllers/userC.js
@@ -21,7 +21,39 @@ class UserC {
   
   //login
   static login(req, res, next) {
-    
+    let unameOrEmail = {}
+    if (req.body.username) {
+      unameOrEmail = req.body.username
+    } else if (req.body.email) {
+      unameOrEmail = req.body.email
+    }
+    User.findOne({
+      where: unameOrEmail
+    })
+      .then((data) => {
+        if (!data) {
+          throw {
+            status: 401,
+            message: "Account not found!"
+          }
+        } else if (compare(req.body.password, data.password)) {
+          const access_token = generateToken({
+            id: data.id,
+            email: data.email
+          })
+          res.status(200).json({
+            id: data.id,
+            email: data.email,
+            access_token
+          })
+        } else {
+          throw {
+            status: 401,
+            message: "Invalid email/password!"
+          }
+        }
+      })
+      .catch(next)
   }
 }
 
