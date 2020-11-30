5de4f3d66559061f6f321208d41c3a83d7dfb850
Hendri Budi S <68056661+hndrbs@users.noreply.github.com>
2020-11-30 23:23:01 +0700

    Revert "initiate, basic crud"

--- server/routers/task-router.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const TaskController = require('../controllers/task-controller')
-
-router.get('/', TaskController.retrieve)
-router.post('/', TaskController.create)
-router.put('/:id', TaskController.update)
-router.delete('/:id', TaskController.delete)
-
-module.exports = router
