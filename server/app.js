b9c3f89ad76b6ef1d537178a777252d69aa23c14
robbykansas <lomiver@gmail.com>
2020-11-30 18:41:54 +0700

    revisi

--- server/app.js
+++ server/app.js
@@ -8,11 +8,13 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const routes = require("./routes")
 
-
 app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(routes)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
