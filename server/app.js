af8a5589364f1322b6a8a729f89b81895a4551a5
icanq <icanq@.localdomain>
2020-11-30 16:52:35 +0700

    add routes, controllers wip

---
+++ server/app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const errorHandler = require('./middlewares/errorHandler.js');
+const router = require('./routes/index.js')
+const cors = require('cors')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors())
+
+app.use(router)
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+  console.log(`listen at`, PORT)
+})
---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+
+router.post('/login', )
+router.post('/register',)
+
+//authentication n authorization
+router.get('/tasks',)
+router.post('/tasks',)
+router.put('/tasks/:id',)
+router.delete('/tasks/:id',)
+  
+module.exports = router;
