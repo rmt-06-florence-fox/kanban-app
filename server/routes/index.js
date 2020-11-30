eff8d496063f5e2b6534b10e473ee5fbd6ecd449
hafis915 <hafistafsani915uh@gmail.com>
2020-11-30 21:04:17 +0800

    try deploy

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,9 @@ const route = require('express').Router()
 const userRoute = require('./userRoute')
 const taskRoute = require('./taskRoute')
 
+route.get('/', (req,res) =>{
+    res.send('hello')
+})
 route.use('/',userRoute)
 route.use('/task',taskRoute)
 module.exports = route
