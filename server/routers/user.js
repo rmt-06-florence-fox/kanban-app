3c0670eefd8845be4be159869117e22f35aedb14
dayuhidayatullah <dayuhidayat123@gmail.com>
2020-11-30 23:40:57 +0700

    test-heroku-test-get

--- server/routers/user.js
+++ server/routers/user.js
@@ -1,6 +1,9 @@
 const router = require('express').Router()
 const User = require('../controllers/user')
 
+router.get('/', (req, res) => {
+    res.send("<h1> Hello World")
+})
 router.post('/register', User.register)
 router.post('/login', User.login)
 
