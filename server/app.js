65d0f2270cc9ca17072fe38d5a8038447fcf2cd5
dayuhidayatullah <dayuhidayat123@gmail.com>
2020-11-30 23:35:01 +0700

    test-heroku

--- server/app.js
+++ server/app.js
@@ -7,8 +7,8 @@ const router = require('./routers')
 const cors = require('cors')
 const errorHandler = require('./middlewares/errorHandler')
 const app = express()
-// const port = process.env.PORT || 3000
-const port = 3000
+const port = process.env.PORT || 3000
+// const port = 3000
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
