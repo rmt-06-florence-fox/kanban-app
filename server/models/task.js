026b20114218003922b2f28554460c9feb8d85d5
normndakbr <normnd.akbr2@gmail.com>
2020-12-02 02:27:28 +0700

    added table relation, validations, hash password on User password

--- server/models/task.js
+++ server/models/task.js
@@ -4,18 +4,26 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Task extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
-      // define association here
+      Task.belongsTo(models.User, {foreignKey: 'UserId', targetKey: 'id'});
     }
   };
   Task.init({
-    title: DataTypes.STRING,
-    category: DataTypes.STRING,
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { message: "Title is required" }
+      }
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { message: "Category is required" }
+      }
+    },
     UserId: DataTypes.INTEGER
   }, {
     sequelize,
--- server/models/user.js
+++ server/models/user.js
@@ -1,24 +1,42 @@
 'use strict';
+const { hash } = require('bcrypt');
 const {
   Model
 } = require('sequelize');
+
+const hashPassword = require("../helpers/bcrypt");
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
-      // define association here
+      User.hasMany(models.Task, {foreignKey: 'UserId'});
     }
   };
   User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: { message: 'Input must be email!' },
+        notEmpty: { message: 'Email is required!'}
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { message: 'Password is required!' }
+      }
+    }
   }, {
     sequelize,
     modelName: 'User',
+    hooks: {
+      beforeCreate(user) {
+        user.password = hashPassword(user.password);
+      }
+    }
   });
   return User;
 };
