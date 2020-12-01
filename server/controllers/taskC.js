489a482f393336900f71d0d29e8ba7c5cfe6c5ab
icanq <icanq@.localdomain>
2020-12-01 13:52:20 +0700

    Task update ✔

--- server/controllers/taskC.js
+++ server/controllers/taskC.js
@@ -1,3 +1,4 @@
+const { where } = require('sequelize/types')
 const { Task } = require('../models')
 
 class TaskC {
@@ -15,9 +16,7 @@ class TaskC {
   static async get(req, res, next) {
     try {
       const data = await Task.findAll({
-        where: {
-          UserId: req.loggedin.id
-        }
+        where: { UserId: req.loggedin.id }
       })
       res.status(200).json(data)
     } catch (error) {
@@ -26,7 +25,15 @@ class TaskC {
   }
   //update
   static async update(req, res, next) {
-    
+    try {
+      const id = +req.params.id;
+      const { title, category } = req.body
+      const data = await Task.update({ title, category }, { where: { id } }, { returning: true })
+      if (!data) throw { status: 404, message: "Data not found!" }
+      else {res.status(200).json(data)}
+    } catch (error) {
+      next(error)
+    }
   }
   //delete
   static async delete(req, res, next) {
