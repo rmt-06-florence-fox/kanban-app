78ea1cda7924312b4b744113f9e859a7725cb65d
fajarrrX <fajarrr30@gmail.com>
2020-12-02 01:28:46 +0700

    second commit: 1. added skeleton server and setting up migrations and models 2. added client index.html template kanban-app and style.css

---
+++ server/models/user.js
@@ -0,0 +1,57 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasMany(models.Task);
+    }
+  }
+  User.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          notNull: {
+            args: true,
+            msg: `Your Email is required!`,
+          },
+          notEmpty: {
+            args: true,
+            msg: `Email must be filled!`,
+          },
+          isEmail: {
+            args: true,
+            msg: `Email must be in format 'yourname@example.com'`,
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            args: true,
+            msg: `Your Password is required!`,
+          },
+          notEmpty: {
+            args: true,
+            msg: `Password must be filled!`,
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
+};
