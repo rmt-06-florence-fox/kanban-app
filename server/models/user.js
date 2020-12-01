6f608b8bb985703b2789136368ae5cb695de7045
taufikikk <taufikikk@gmail.com>
2020-12-01 22:31:05 +0700

    Client: 1. Add icon to head
    Server:
    1. Remove comment code

--- server/models/user.js
+++ server/models/user.js
@@ -5,13 +5,7 @@ const {
 const {hashPassword} = require('../helpers/bcrypt')
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
     }
   };
   User.init({
