b71bdbb9f33764af571411162a5b60df34cbc9a7
tiorzal <tioralmunazir@gmail.com>
2020-12-01 14:36:02 +0700

    1. nambah navbar 2. rapihin deklarasi env

--- server/app.js
+++ server/app.js
@@ -1,1 +1,20 @@
-const express = require('express')
+if (process.env.NODE_ENV !== 'production'){
+  console.log('a');
+  require('dotenv').config()
+}
+
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 3000
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended : true }))
+app.use(express.json())
+
+app.get('/', (req,res)=>{ res.send('adsda') })
+
+
+app.listen(port, ()=>{
+  console.log(port);
+})
