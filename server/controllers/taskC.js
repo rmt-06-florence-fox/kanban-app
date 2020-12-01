e9f59cdba473a400ff670164263feda8c3ec67e6
icanq <icanq@.localdomain>
2020-12-01 13:46:16 +0700

    Task read ✔

--- server/controllers/taskC.js
+++ server/controllers/taskC.js
@@ -13,7 +13,16 @@ class TaskC {
   }
   //read
   static async get(req, res, next) {
-    
+    try {
+      const data = await Task.findAll({
+        where: {
+          UserId: req.loggedin.id
+        }
+      })
+      res.status(200).json(data)
+    } catch (error) {
+      next(error)
+    }
   }
   //update
   static async update(req, res, next) {
