dcab93ccaf58dd2c2ceab42cae753691ad8b60bb
taufikikk <taufikikk@gmail.com>
2020-12-02 08:56:53 +0700

    Server: 1. Add Google Login on Controller and Routes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,8 @@
 const { comparePassword } = require('../helpers/bcrypt')
 const { generateToken } = require('../helpers/jwt')
 const { User } = require('../models')
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 class UserController {
     static async register(req, res, next) {
@@ -36,6 +38,29 @@ class UserController {
                     }
                 }
             }
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async googleLogin(req, res, next){
+        try {
+            let ticket = await client.verifyIdToken({
+                idToken: req.body.googleToken,
+                audience: process.env.GOOGLE_CLIENT_ID
+            })
+            let payload = await ticket.getPayload()
+            let user = await User.findOne({where:{email: payload.email}})
+            if(user){
+                await user
+            } else {
+                user = await User.create({
+                    email: payload.email,
+                    password: process.env.GOOGLE_LOGIN_PASS
+                })
+            }
+            const access_token = generateToken({id: user.id, email: user.email})
+            res.status(200).json({access_token})
         } catch (error) {
             next(error)
         }
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,5 +3,7 @@ const UserController = require('../controllers/user')
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
+router.post('/googlelogin', UserController.googleLogin)
+
 
 module.exports = router
