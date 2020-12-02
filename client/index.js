5f4fd3a13b8a9b43f527a035ff107d7097d1274e
adhim1st <adhim@student.uns.ac.id>
2020-12-02 13:45:29 +0700

    client with vue.js

--- client/index.js
+++ client/index.js
@@ -11,3 +11,61 @@ function modalEdit() {
   const instance = M.Modal.getInstance($("#modal-edit"));
   instance.open();
 }
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    currentPage: "kanban-page",
+    backlog: [
+      {
+        id: "1",
+        title: "server",
+        description: "coding routing",
+      },
+      {
+        id: "2",
+        title: "client",
+        description: "coding frontend",
+      },
+    ],
+    product: [
+      {
+        id: "1",
+        title: "kanban main page",
+        description: "kanban main page with materialize",
+      },
+      {
+        id: "2",
+        title: "kanban login page",
+        description: "kanban login page with post routing",
+      },
+    ],
+    development: [
+      {
+        id: "1",
+        title: "configuring database",
+        description: "creating database with postgres",
+      },
+      {
+        id: "2",
+        title: "migrating table",
+        description: "creating table name and attributes",
+      },
+    ],
+    done: [
+      {
+        id: "1",
+        title: "forking github",
+        description: "done forking origanization repository",
+      },
+    ],
+  },
+  methods: {
+    showLogin() {
+      this.currentPage = "login-page";
+    },
+    showRegister() {
+      this.currentPage = "register-page";
+    },
+  },
+});
