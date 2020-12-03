3652e85dae63a42831cf2c0558ef07f461512d87
FebrianAditya <febrian.aditya@student.upi.edu>
2020-12-03 08:46:24 +0700

    apllication vue in register-page, login-page, main-page, and add axios. But still have bug, if i refresh, homepage will back to home-page

--- client/template/main.js
+++ client/template/main.js
@@ -2,11 +2,73 @@ var app = new Vue({
     el: '#app', // ini element (ini akan merender apapung yang ada dibawah id app)
     data: {
       message: 'Hello Vue!',
-      pageName: "home-page"
+      pageName: "home-page",
+      dataUser: {
+        email: "",
+        password: ""
+      },
+      dataUserRegister: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+
+      }
     },
     methods: {
         changePage(value) {
             this.pageName = value
+        },
+        loginData() {
+          // console.log(this.dataUser)
+          axios({
+            url: "http://localhost:3000/login",
+            data: this.dataUser,
+            method: "post"
+          })
+            .then(data => {
+              // console.log(data.data.acces_token)
+                const acces_token = data.data.acces_token
+                localStorage.setItem("acces_token", acces_token)
+                this.changePage("kanban-page")
+                this.dataUser.email = ""
+                this.dataUser.password = ""
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        },
+        logOut() {
+          localStorage.clear()
+          this.changePage("home-page")
+        },
+        ifHomePage() {
+            // return "home-page"
+          // if(localStorage.getItem("acces_token")) {
+          //   return "kanban-page"
+          // } else {
+          //   return "home-page"
+          // }
+        },
+        registerData() {
+          console.log(this.dataUserRegister)
+          axios({
+            url: "http://localhost:3000/register",
+            data: this.dataUserRegister,
+            method: "post"
+          })
+            .then(data => {
+              // console.log(data.data.acces_token)
+                
+                this.changePage("form-login")
+                this.dataUserRegister.email = ""
+                this.dataUserRegister.password = ""
+                this.dataUserRegister.firstName = ""
+                this.dataUserRegister.lastName = ""
+            })
+            .catch(err => {
+                console.log(err)
+            })
         }
     }
   })
