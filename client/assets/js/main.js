c4224d6b9577fed136b6a02d11884c34ea741f74
yopikurniawan <yopikprabowo@gmail.com>
2020-12-03 13:23:58 +0700

    still hardcode vue directive and append error-page

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,6 +1,41 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    pageName: 'error-page',
+    tasks: [
+      {
+        title: 'compile A/B'
+      },
+      {
+        title: 'deploy client'
+      },
+      {
+        title: 'testing server'
+      },
+      {
+        title: 'compile A/B'
+      },
+      {
+        title: 'deploy client'
+      },
+      {
+        title: 'testing server'
+      }
+    ],
+    categories: [
+      {
+        name: 'Backlog'
+      },
+      {
+        name: 'Todo'
+      },
+      {
+        name: 'Doing'
+      },
+      {
+        name: 'Done'
+      }
+    ]
   }
 })
