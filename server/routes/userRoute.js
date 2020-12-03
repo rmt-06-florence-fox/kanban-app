a09138a3d82d791e5741f1ed29a4c7878ad20eb7
hafis915 <hafistafsani915uh@gmail.com>
2020-12-03 21:05:53 +0800

    tambahkan route untuk google oauth

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,6 +3,7 @@ const {UserController} = require('../controller')
 
 route.post('/register', UserController.register)
 route.post('/login', UserController.login)
+route.post('/googlelogin',UserController.googleLogin)
 // route.post('/signIn', UserController.signInUser)
 
 module.exports = route
