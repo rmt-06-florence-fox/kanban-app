e15ed71058d76e9c97ddaa7e870f66164a975407
pranotoism <bayupranotodwiputra@gmail.com>
2020-12-05 05:48:59 +0700

    success message for update and login

--- client/src/App.vue
+++ client/src/App.vue
@@ -93,6 +93,22 @@ export default {
           }
         })
         .then(response => {
+          const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+
+          Toast.fire({
+              icon: 'success',
+              title: 'A task has been updated successfully'
+          })
           this.currentPage = 'home'
         })
         .catch(err => {
