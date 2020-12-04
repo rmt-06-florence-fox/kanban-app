25603e686ec95326779e1a09fe1800b652e6842c
taufiqgiri <taufiqgiri.math@gmail.com>
2020-12-05 00:54:04 +0700

    memisahkan component dan persiapan deploy ulang

--- client-component/src/components/MainPage.vue
+++ client-component/src/components/MainPage.vue
@@ -1,31 +1,8 @@
 <template>
     <div>
-    <Navbar @changePage="changePage"></Navbar>
-    <AddCategory @addCategory="addCategory"></AddCategory>
-    <div class="container-category container-fluid">
-        <div class="row row-category">
-            <div v-for="(category, index) in categories" :key="index" class="col-sm-3">
-                <div class="bg-warning text-white title-category">
-                    <h6 class="pointer"><a @click.prevent="addKanban(category)" href="">{{category.name}}</a></h6>
-                </div>
-                <div v-for="(task, index) in tasks" :key="index" class="category">
-                    <div v-if="task.CategoryId === category.id" class="bg-primary text-white desc-category card">
-                        <div>
-                            {{task.title}}
-                        </div>
-                        <div>
-                            by {{name}}
-                        </div>
-                        <div class="category-symbol">
-                            <a @click.prevent="editKanban(task)" href=""><i class="material-icons pointer" style="font-size:12px">mode_edit</i></a>
-                            <p></p>
-                            <a @click.prevent="deleteKanban(task)" href=""><i class="material-icons pointer" style="font-size:12px">delete</i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+        <Navbar @changePage="changePage"></Navbar>
+        <AddCategory @addCategory="addCategory"></AddCategory>
+        <BoardList :tasks="tasks" :categories="categories" @addKanban="addKanban" @editKanban="editKanban" @deleteKanban="deleteKanban"></BoardList>
     </div>
 
 </template>
@@ -33,18 +10,20 @@
 <script>
 import Navbar from "./Navbar"
 import AddCategory from "./AddCategory"
+import BoardList from "./BoardList"
 
 export default {
     name: "MainPage",
     props: [ 'tasks', 'categories' ],
     data() {
         return {
-            name: localStorage.getItem('name')
+            
         }
     },
     components: {
         Navbar,
-        AddCategory
+        AddCategory,
+        BoardList
     },
     methods: {
         changePage(page) {
@@ -53,14 +32,14 @@ export default {
         addCategory(name) {
             this.$emit("addCategory", name)
         },
-        addKanban(category) {
-            this.$emit("addKanban", "Add Kanban Page", category.id)
+        addKanban(page, id) {
+            this.$emit("addKanban", page, id)
         },
-        editKanban(task) {
-            this.$emit("editKanban", "Edit Kanban Page", task)
+        editKanban(page, task) {
+            this.$emit("editKanban", page, task)
         },
-        deleteKanban(task) {
-            this.$emit("deleteKanban", task.id)
+        deleteKanban(id) {
+            this.$emit("deleteKanban", id)
         }
     }
 }
---
+++ client-component/src/components/TaskBoard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="bg-warning text-white title-category">
+            <h6 class="pointer"><a @click.prevent="addKanban(category)" href="">{{category.name}}</a></h6>
+        </div>
+        <div v-for="(task, index) in tasks" :key="index" class="category">
+        <TaskItem :task="task" :category="category" @editKanban="editKanban" @deleteKanban="deleteKanban"></TaskItem>
+        </div>
+    </div>
+</template>
+
+<script>
+import TaskItem from "./TaskItem"
+
+export default {
+    name: "TaskBoard",
+    props: [ 'tasks', 'category' ],
+    components: {
+        TaskItem
+    },
+    methods: {
+        addKanban(category) {
+            this.$emit("addKanban", "Add Kanban Page", category.id)
+        },
+        editKanban(page, task) {
+            this.$emit("editKanban", page, task)
+        },
+        deleteKanban(id) {
+            this.$emit("deleteKanban", id)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
