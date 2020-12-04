5bdbfaf91161a690830f159a09292ed229e7a92f
reyhanprtrs <reyhanprtrs@gmail.com>
2020-12-04 23:12:12 +0700

    latest update

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const userRouter = require('./userroute')
 const taskRouter = require('./taskroute')
 
+router.get('/', (req, res) => res.send('Hello'))
 router.use('/', userRouter)
 router.use('/tasks', taskRouter)
 
