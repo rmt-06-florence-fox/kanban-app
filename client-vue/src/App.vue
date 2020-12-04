84be09711acfafe51566fbf190407904ffaebe3a
tiorzal <tioralmunazir@gmail.com>
2020-12-04 14:26:34 +0700

    1. add google log out function

--- client-vue/src/App.vue
+++ client-vue/src/App.vue
@@ -123,6 +123,11 @@ export default {
         text : `byeeee`
       })
       localStorage.removeItem('access_token')
+      var auth2 = gapi.auth2.getAuthInstance();
+      auth2.signOut().then(function () {
+        console.log('User signed out.');
+      });
+
       this.currentPage = page;
     },
     async fetch(){
