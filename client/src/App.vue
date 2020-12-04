33182f1b8ac419240cfa1954023cd82f80c30ebc
yopikurniawan <yopikprabowo@gmail.com>
2020-12-04 09:57:59 +0700

    append fonts

---
+++ client/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <LoginPage></LoginPage>
+</template>
+
+<script>
+import LoginPage from './components/LoginPage'
+export default {
+  name: 'App',
+  data() {
+    return {
+      danbam: 'nice shoot'
+    }
+  },
+  components: {
+    LoginPage
+  }
+}
+</script>
+
+<style>
+
+</style>
