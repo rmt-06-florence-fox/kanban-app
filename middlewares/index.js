531b2ef5c5a78fd3c608aa4be1609ffce589cf89
muchsin28 <hab@muchsin.id>
2020-12-07 06:04:59 +0700

    resolve duplicate file

--- middlewares/index.js
+++
@@ -1,9 +0,0 @@
-const Authentication= require('./authentication')
-const Authorization= require('./authorization')
-const ErrorHandler= require('./errorhandler')
-
-module.exports= {
-  Authentication,
-  Authorization,
-  ErrorHandler
-}
