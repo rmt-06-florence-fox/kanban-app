cdb0c855993f07802e0eadabb8317d2b5db27f98
yopikurniawan <yopikprabowo@gmail.com>
2020-12-18 10:59:45 +0700

    process deploy client

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://danbam-ykp.herokuapp.com'
 })
 
 export default instance
